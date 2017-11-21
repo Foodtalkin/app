1f1d3b8a8c05efaa9ffc571589f2dd9e2dae0dcc
rchshrm16 <rchshrm16@gmail.com>
2017-11-21 12:54:49 +0530

    paytm fix

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,9 +4,26 @@
  * ============================================================ */
 
 angular.module('app')
-    .config(['$stateProvider', '$urlRouterProvider', '$ocLazyLoadProvider',
+    .config(['$stateProvider', '$urlRouterProvider', '$ocLazyLoadProvider','$httpProvider',
 
-        function($stateProvider, $urlRouterProvider, $ocLazyLoadProvider) {
+        function($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, $httpProvider) {
+
+            // remove chaching in browser
+            if (!$httpProvider.defaults.headers.get) {
+                $httpProvider.defaults.headers.get = {};    
+            }    
+
+            // Answer edited to include suggestions from comments
+            // because previous version of code introduced browser-related errors
+
+            //disable IE ajax request caching
+            // $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
+            // extra
+            $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+            $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
+
+
+
             $urlRouterProvider
                 .otherwise('/app/home');
 
